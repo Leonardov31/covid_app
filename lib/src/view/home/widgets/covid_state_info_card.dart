2b8a925a44a4b1b39e3d6622a0a67949d1dd11a3
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 10:34:50 -0300

    Create covid_state_info_card.dart

---
+++ lib/src/view/home/widgets/covid_state_info_card.dart
@@ -0,0 +1,69 @@
+import 'package:covid_app/src/view/citys/citys.dart';
+import 'package:flutter/material.dart';
+
+import '../../../app/app_text_style.dart';
+import '../../../viewmodels/covid_data_view_model.dart';
+
+class CovidStateInfoCard extends StatelessWidget {
+  const CovidStateInfoCard({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final CovidDataViewModel data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () => nextPage(context),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("Estado - ${data.state}", style: AppTextStyle.title),
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${data.confirmed}",
+                        style: AppTextStyle.bodyTitle.copyWith(
+                          color: Colors.teal[800],
+                        ),
+                      ),
+                      const Text("casos confirmados"),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${data.deaths}",
+                        style: AppTextStyle.bodyTitle.copyWith(
+                          color: Colors.red[800],
+                        ),
+                      ),
+                      const Text("mortes confirmadas"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void nextPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Citys(state: data.state)),
+    );
+  }
+}
