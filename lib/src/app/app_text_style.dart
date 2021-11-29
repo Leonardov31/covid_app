d03fe69b9a2a00e368ae103e0461c8720efbcb41
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 10:33:54 -0300

    Create app_text_style.dart

---
+++ lib/src/app/app_text_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyle {
+  static final title = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    color: Colors.blue[900],
+  );
+
+  static final bodyTitle = GoogleFonts.montserrat(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+}
