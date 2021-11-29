890f14d43c71d42090f295bce732ecd85c112215
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 12:49:40 -0300

    Update app_theme.dart

--- lib/src/app/app_theme.dart
+++ lib/src/app/app_theme.dart
@@ -5,5 +5,27 @@ class AppTheme {
   static ThemeData theme = ThemeData(
     primarySwatch: Colors.blue,
     fontFamily: GoogleFonts.montserrat().fontFamily,
+    inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        primary: Colors.blue[900],
+        padding: const EdgeInsets.all(10.0),
+        textStyle: const TextStyle(fontSize: 18),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        elevation: 0,
+        primary: Colors.blue[900],
+        padding: const EdgeInsets.all(10.0),
+        textStyle: const TextStyle(fontSize: 18),
+      ),
+    ),
   );
 }
