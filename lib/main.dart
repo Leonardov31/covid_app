d2625accb2663a34cfd8ee4c0778e0f9ed0906e4
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 10:39:01 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'src/app_widget.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Covid App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Covid app"),
-      ),
-    );
-  }
-}
+void main() => runApp(const MyApp());
