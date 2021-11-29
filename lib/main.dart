111860654bb523237f6eb41b7ece6687ccebcae5
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 12:49:29 -0300

    Firebase Auth setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app_widget.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
