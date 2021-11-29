b655dbd900f1d3a5694789eb5dbe09760d4bff71
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 12:50:32 -0300

    Add AuthService to MultiProvider

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,18 +1,47 @@
+import 'package:covid_app/src/services/auth_service.dart';
+import 'package:covid_app/src/view/auth/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'app/app_theme.dart';
-import 'view/home/home.dart';
+import 'view/home/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Covid App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.theme,
-      home: const Home(),
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+          create: (_) => AuthService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<AuthService>().authState,
+          initialData: null,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Covid App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.theme,
+        home: const Authenticate(),
+      ),
     );
+  }
+}
+
+class Authenticate extends StatelessWidget {
+  const Authenticate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User?>();
+
+    if (firebaseUser != null) {
+      return const HomePage();
+    }
+    return LoginPage();
   }
 }
