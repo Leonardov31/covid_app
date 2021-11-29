05a9b5a9e36c77af5d133baddea485426fd6ce69
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-11-29 10:33:59 -0300

    Create app_theme.dart

---
+++ lib/src/app/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData(
+    primarySwatch: Colors.blue,
+    fontFamily: GoogleFonts.montserrat().fontFamily,
+  );
+}
